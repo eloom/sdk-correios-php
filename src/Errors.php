7ae1433f84d43ee792311953e899c1cd419b28f4
Rodrigo Sanders <contato@eloom.com.br>
2023-09-19 22:53:24 -0300

    Create Errors.php

---
+++ src/Errors.php
@@ -0,0 +1,24 @@
+<?php
+declare(strict_types=1);
+
+namespace Correios;
+
+class Errors {
+
+	private static $errors = [
+		'999' => 'Erro inesperado.',
+		'001' => 'Falha na conexão com os Correios. Por favor, tente mais tarde.',
+		'002' => 'País de origem/destino deve ser Brasil.',
+		'003' => 'Código Postal da Loja está incorreto.',
+		'004' => 'Dimensões não encontradas para o produto %s.'
+	];
+
+	public static function getMessage($code) {
+		if (array_key_exists($code, self::$errors)) {
+			return self::$errors[$code];
+		}
+		
+		return self::$errors['999'];
+	}
+
+}
