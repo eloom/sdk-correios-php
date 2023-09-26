21c4c9c989367ed1bf2c7a61349adbafc57e03af
Rodrigo Sanders <contato@eloom.com.br>
2023-09-26 16:39:07 -0300

    Update test.php

--- test.php
+++ test.php
@@ -15,4 +15,21 @@ try {
 } catch (\Throwable $e) {
     echo $e->getMessage();
 }
+
+$client->prazo()->withProduct('04162')->withProduct('04669')->withCepOrigem('88330725')->withCepDestino('92010130')->nacional();
+
+$client->preco()->withProduct('04162')
+->withProduct('04669')
+->withCepOrigem('88330725')
+->withCepDestino('92010130')
+->withPsObjeto('300')
+->withTpObjeto('2')
+->withComprimento('20')
+->withLargura('20')
+->withAltura('20')
+->withDiametro('20')
+->withDtEvento(date("d/m/Y"))
+->nacional();
+
+$client->rastro()->withCodigoObjeto('NL832806015BR')->withResultado(Rastro::EVENTOS_TODOS)->objeto();
 ?>
