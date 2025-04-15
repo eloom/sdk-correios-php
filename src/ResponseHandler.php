fffe5019d66d044087c19038ac0c40447b981580
élOOm <contato@eloom.com.br>
2025-04-15 16:31:16 -0300

    Update ResponseHandler.php

--- src/ResponseHandler.php
+++ src/ResponseHandler.php
@@ -2,90 +2,90 @@
 
 namespace Eloom\SdkCorreios;
 
-use GuzzleHttp\Exception\ClientException;
 use Eloom\SdkCorreios\Exceptions\CorreiosException;
 use Eloom\SdkCorreios\Exceptions\InvalidJsonException;
 use Eloom\SdkCorreios\Exceptions\UnauthorizedException;
+use GuzzleHttp\Exception\ClientException;
 
 class ResponseHandler {
 
-    /**
-     * @param string $payload
-     *
-     * @throws InvalidJsonException
-     * @return \ArrayObject
-     */
-    public static function success($payload) {
-        return self::toJson($payload);
-    }
+	/**
+	 * @param string $payload
+	 *
+	 * @return \ArrayObject
+	 * @throws InvalidJsonException
+	 */
+	public static function success($payload) {
+		return self::toJson($payload);
+	}
 
-    /**
-     * @param ClientException $originalException
-     *
-     * @throws PagarMeException
-     * @return void
-     */
-    public static function failure(\Exception $originalException) {
-        throw self::parseException($originalException);
-    }
+	/**
+	 * @param ClientException $originalException
+	 *
+	 * @return void
+	 * @throws PagarMeException
+	 */
+	public static function failure(\Exception $originalException) {
+		throw self::parseException($originalException);
+	}
 
-    /**
-     * @param ClientException $guzzleException
-     *
-     * @return CorreiosException|ClientException
-     */
-    private static function parseException(ClientException $guzzleException) {
-        $response = $guzzleException->getResponse();
+	/**
+	 * @param ClientException $guzzleException
+	 *
+	 * @return CorreiosException|ClientException
+	 */
+	private static function parseException(ClientException $guzzleException) {
+		$response = $guzzleException->getResponse();
 
-				die(print_r($response, true));
+		die(print_r($response, true));
 
-        if (is_null($response)) {
-            return $guzzleException;
-        }
+		if (is_null($response)) {
+			return $guzzleException;
+		}
 
-        if($response->getStatusCode() == 401) {
-            return new UnauthorizedException('401 Unauthorized', date("d/m/Y"), '/v1/autentica/cartaopostagem');
-        }
+		if ($response->getStatusCode() == 401) {
+			return new UnauthorizedException('401 Unauthorized', date("d/m/Y"), '/v1/autentica/cartaopostagem');
+		}
 
-        $body = $response->getBody()->getContents();
+		if ($response->getStatusCode() == 400) {
+			$error = null;
 
-        $jsonError = null;
-        
-        try {
-            $jsonError = self::toJson($body);
-        } catch (InvalidJsonException $invalidJson) {
-            return $guzzleException;
-        }
+			if (is_array($jsonError)) {
+				$error = $jsonError[0];
+			} else {
+				$error = $jsonError;
+			}
+			if (null != $error) {
+				if ($error->txErro) {
+					return new CorreiosException($error->txErro, date("d/m/Y"), '');
+				}
+			}
+		}
 
-        if($response->getStatusCode() == 400) {
-            $error = null;
+		$body = $response->getBody()->getContents();
 
-            if(is_array($jsonError)) {
-                $error = $jsonError[0];
-            } else {
-                $error = $jsonError;
-            }
-            if(null != $error) {
-                if($error->txErro) {
-                    return new CorreiosException($error->txErro, date("d/m/Y"), '');
-                }
-            }
-        }
+		$jsonError = null;
 
-        return new CorreiosException($jsonError->msgs, $jsonError->date, $jsonError->path);
-    }
+		try {
+			$jsonError = self::toJson($body);
+		} catch (InvalidJsonException $invalidJson) {
+			return $guzzleException;
+		}
 
-    /**
-     * @param string $json
-     * @return \ArrayObject
-     */
-    private static function toJson($json) {
-        $result = json_decode($json);
+		return new CorreiosException($jsonError->msgs, $jsonError->date, $jsonError->path);
+	}
 
-        if (json_last_error() != \JSON_ERROR_NONE) {
-            throw new InvalidJsonException(json_last_error_msg());
-        }
+	/**
+	 * @param string $json
+	 * @return \ArrayObject
+	 */
+	private static function toJson($json) {
+		$result = json_decode($json);
 
-        return $result;
-    }
+		if (json_last_error() != \JSON_ERROR_NONE) {
+			throw new InvalidJsonException(json_last_error_msg());
+		}
+
+		return $result;
+	}
 }
